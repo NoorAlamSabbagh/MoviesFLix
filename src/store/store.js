85d2ef5d5c6b1c9888423fe6a4499d1d0743f44b
Noor Alam <alamrangrej787@gmail.com>
2023-07-29 12:58:58 +0530

    comp

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-export default configureStore({
-  reducer: {},
+import homeSlice from "./homeSlice";
+
+export const store = configureStore({
+    reducer: {
+        home: homeSlice,
+    },
 });
